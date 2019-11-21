2dcbe0d9583678f9d1db748f47cede3adba01da9
Steven Smith <smith84@llnl.gov>
2019-11-21 09:48:11 -0800

    Added singularity to parflow runner script

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+default:
+	./bin/parflow.sh default_single.tcl 1 1 1
+
+clean:
+	rm -f default_single.out.*
--- bin/parflow.sh
+++ bin/parflow.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run --rm -v $(pwd):/data parflow/parflow $*
+if docker ps >& /dev/null
+then
+   docker run --rm -v $(pwd):/data parflow/parflow $*
+elif singularity version >& /dev/null
+then
+   singularity run docker://parflow/parflow $*
+else
+   echo "Couldn't run docker or singularity"
+fi
