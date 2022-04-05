4465a3c3cd67d6e311c504ebce5310ee30fa7ea4
Steven Smith <smith84@llnl.gov>
2022-04-04 20:55:29 -0700

    Updated release

--- bin/parflow.sh
+++ bin/parflow.sh
@@ -2,10 +2,10 @@
 
 if docker ps >& /dev/null
 then
-   docker run --rm -v $(pwd):/data parflow/parflow:version-3.8.0 $*
+   docker run --rm -v $(pwd):/data parflow/parflow:version-3.10.0 $*
 elif singularity version >& /dev/null
 then
-   singularity run docker://parflow/parflow:version-3.8.0 $*
+   singularity run docker://parflow/parflow:version-3.10.0 $*
 else
    echo "Couldn't run docker or singularity"
 fi
